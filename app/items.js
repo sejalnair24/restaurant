066dda4fa395bfdf6570042f2ede9fe6e7347b72
VaishnaviUpadyaya <upadyayavaishnavi10@gmail.com>
2025-11-06 23:23:08 +0530

    food array to be used in different pages

---
+++ app/items.js
@@ -0,0 +1,9 @@
+export const food=[
+{title:"Braised Chicken Legs",description:"4 chicken legs *  Chilli Sauce * Soft Drinks",image:"/BCL.jpg"},
+{title:"Bone Steak",description:"4 chicken legs *  Chilli Sauce * Soft Drinks",image:"/BS.jpg"}, 
+{title:"Fish Tacos with Chipotle Crema",description:"4 chicken legs *  Chilli Sauce * Soft Drinks",image:"/FTCC.jpg"},
+{title:"Broken Lasagna & Parmesan",description:"4 chicken legs *  Chilli Sauce * Soft Drinks",image:"/BLP.jpg"},
+{title:"Seared Scallops with Butter",description:"4 chicken legs *  Chilli Sauce * Soft Drinks",image:"/SSB.jpg"},
+{title:"Double-Stack Mushroom",description:"4 chicken legs *  Chilli Sauce * Soft Drinks",image:"/DSM.jpg"},
+
+   ];
