d05316d7318edd7851222672da07464b3f57eb4a
VaishnaviUpadyaya <upadyayavaishnavi10@gmail.com>
2025-11-08 23:09:11 +0530

    Login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,8 +1,12 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { login, createAccount, logout, resetEmail } from "../core/auth";
+
+import { useState } from "react";
 import Footer from "../components/Footer";
 export default function Login()
-{
+{const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
          const router=useRouter();
     return(
      
@@ -48,13 +52,26 @@ export default function Login()
                   <div className="md:ml-130 ml-10">  <h1 className="text-black text-3xl font-bold  md:mt-25 mt-90 ">WELCOME BACK</h1></div>
                    <div className="md:-ml-63 ml-11"> <h6 className="text-gray-700  md:mt-38 mt-5 ">Enter your email and password</h6></div>
                     <div className="flex flex-col md:-ml-56 ml-10">
-                        <div className="md:w-90 w-70 h-13 bg-gray-300 rounded-[5px] md:mt-50 mt-5 ">
-                        <h1 className=" ml-5 mt-3 ">Email*</h1></div>
-                         <div className="md:w-90 w-70 h-13 bg-gray-300 rounded-[5px] mt-5 ">
-                        <h1 className=" ml-5 mt-3 ">Password*</h1></div>
-                        <div className="md:w-90 w-70 h-13 bg-gray-300 rounded-[5px] mt-5  flex justify-center items-center bg-[#7b4c2b]  hover:bg-yellow-600">
-                        <h1 className="-ml-5">Login</h1></div>
-                       <div className=" flex flex-row ">  <h6 className="text-gray-700 md:ml-0 ml-2 mt-5 ">Don't have an account?</h6><h6 className="text-red-500 underline mt-5 ml-2"> Register Now!</h6></div>
+                        <input className="md:w-90 w-70 h-13 bg-gray-300 text-black rounded-[5px] md:mt-50 mt-5 px-7 "type="email"placeholder="Email*" onChange={(e) => setEmail(e.target.value)}
+                        />
+                         <input className="md:w-90 w-70 h-13 bg-gray-300 text-black rounded-[5px] mt-5 items-center justify-center px-7 " type="password"placeholder="xxxxxx"onChange={(e)=>setPassword(e.target.value)}
+                       />
+                        <button className="md:w-50 w-50 h-13 bg-gray-800 rounded-[5px] mt-5   justify-center items-center mx-20   hover:bg-[#7b4c2b]"onClick={async(event)=>{
+                          console.log(email,password);
+                          await login (email,password);
+                        }}>Login
+                      </button>
+                      <button className="md:w-50 w-70 h-13 bg-gray-800 rounded-[5px] mt-5   justify-center items-center mx-20  hover:bg-[#7b4c2b]"onClick={async(event)=>{
+                          console.log(email,password);
+                          await createAccount (email,password);
+                        }}>Sign Up
+                      </button>
+                      <button className="md:w-50 w-70 h-13 bg-gray-800 rounded-[5px] mt-5   justify-center items-center mx-20  hover:bg-[#7b4c2b]"onClick={async(event)=>{
+                          console.log(email,password);
+                          await resetEmail(email,password);
+                        }}>Send Reset Email
+                      </button>
+                       <div className=" flex flex-row ">  <h6 className="text-gray-700 md:ml-0 ml-2 mt-5 "> Don't have an account?</h6><h6 className="text-red-500 underline mt-5 ml-2"> Register Now!</h6></div>
                     </div>
                     
                  
